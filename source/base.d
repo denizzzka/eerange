c8a89a3f7c123c739de9f7925a2152f5ddcd422b
Denis Feklushkin <denis.feklushkin@gmail.com>
2019-03-19 00:01:12 +0700

    initial

---
+++ source/base.d
@@ -0,0 +1,86 @@
+/// "Each with each other" random range
+module eerange.base;
+
+@safe:
+
+///
+@nogc struct EachWithEachOtherRangeBase(R)
+{
+    private R srcRange;
+
+    ///
+    this(R r)
+    {
+        srcRange = r;
+    }
+
+    ///
+    size_t length() const pure
+    {
+        const len = srcRange.length;
+
+        return (len * len - len) / 2;
+    }
+
+    private static struct Coords
+    {
+        size_t x;
+        size_t y;
+    }
+
+    private Coords coordsInSquare(size_t idx) const pure
+    {
+        return Coords(
+            idx % srcRange.length,
+            idx / srcRange.length
+        );
+    }
+
+    import std.range.primitives: ElementType;
+
+    private alias T = ElementType!R;
+
+    private T[2] getElemBySquareCoords(Coords c) const pure
+    {
+        return [srcRange[c.x], srcRange[c.y]];
+    }
+
+    ///
+    T[2] opIndex(size_t idx) const pure
+    {
+        assert(idx < length);
+
+        Coords coords = coordsInSquare(idx);
+
+        if(coords.x <= coords.y) // under diagonal line?
+        {
+            const latestIdx = srcRange.length - 1;
+
+            // Mirroring coords
+            coords.x = latestIdx - coords.x;
+            coords.y = latestIdx - coords.y - 1; // shifted above diagonal
+        }
+
+        return getElemBySquareCoords(coords);
+    }
+}
+
+unittest
+{
+    import std.parallelism;
+
+    int[] arr = [100, 200, 300, 400];
+
+    auto r = EachWithEachOtherRangeBase!(int[])(arr);
+
+    size_t cnt;
+
+    foreach(i; 0 .. r.length)
+    {
+        auto pair = r[i];
+
+        cnt++;
+    }
+
+    assert(cnt == 6);
+}
---
+++ source/randomrange.d
@@ -0,0 +1,98 @@
+/// "Each with each other" random range
+module eerange.randomrange;
+
+import eerange.base;
+
+@safe:
+
+///
+@nogc struct EachWithEachOtherRandomAccessRange(R)
+{
+    EachWithEachOtherRangeBase!R base;
+    alias base this;
+
+    private size_t fwdIdx = 0;
+    private size_t backIdx;
+
+    ///
+    this(R r)
+    {
+        base = EachWithEachOtherRangeBase!R(r);
+
+        backIdx = length - 1;
+    }
+
+    ///
+    bool empty() const
+    {
+        return fwdIdx >= length || backIdx < 0;
+    }
+
+    auto front() {
+        version(D_NoBoundsChecks){}
+        else
+            assert(!empty);
+
+        return opIndex(fwdIdx);
+    }
+
+    auto back() {
+        version(D_NoBoundsChecks){}
+        else
+            assert(!empty);
+
+        return opIndex(backIdx);
+    }
+
+    ///
+    void popFront() { fwdIdx++; }
+
+    ///
+    void popBack() { backIdx--; }
+
+    ///
+    EachWithEachOtherRandomAccessRange!R save() { return this; }
+}
+
+///
+auto eeRandomRange(T)(T inputRange) pure
+{
+    return EachWithEachOtherRandomAccessRange!(T)(inputRange);
+}
+
+unittest
+{
+    import std.range.primitives;
+    import std.traits;
+
+    alias R = EachWithEachOtherRandomAccessRange!(int[]);
+
+    static assert(is(typeof(lvalueOf!R[1]) == ElementType!R));
+    static assert(isInputRange!R);
+    static assert(is(ReturnType!((R r) => r.save) == R));
+    static assert(isForwardRange!R);
+    static assert(isBidirectionalRange!R);
+    static assert(isRandomAccessRange!R);
+}
+
+@trusted unittest
+{
+    import std.range: iota;
+    import std.parallelism;
+
+    enum testSize = 100;
+
+    auto eeRandom = iota(0, testSize).eeRandomRange;
+    auto randomRes = taskPool.amap!("a[0]", "a[1]")(eeRandom);
+
+    size_t[testSize] cnt;
+
+    foreach(r; randomRes)
+    {
+        cnt[r[0]]++;
+        cnt[r[1]]++;
+    }
+
+    foreach(r; cnt)
+        assert(r == testSize-1);
+}
